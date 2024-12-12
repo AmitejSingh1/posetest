e6abab63432c02c0185c910bd644bce99c40de7f
AmitejSingh1 <amitejsingh5@gmail.com>
2024-12-12 21:08:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def process_frame():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
